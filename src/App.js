81e6215f34792c62a65dcb449720662604489d80
senthil <senthilnathan14062000@gmail.com>
2023-02-21 12:23:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useState } from 'react';
+import './App.scss';
+import Dropdown from './DependentDropdown/Dropdown';
+import List from './List';
+import Select from './List/Select';
+import Multiselect from './MultiSelect/Multiselect';
 
+import Table from './Table';
+
+const options = [
+  { label: "TonyStart", value: 1 },
+  { label: "ChrisEvent", value: 2 },
+  { label: "ChirsHemsworth", value: 3 },
+  { label: "MarkRuffalo", value: 4 },
+  { label: "Jinner", value: 5 },
+]
 function App() {
+  const [value1,setValue1] = useState([options[0]])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <Select  
+        multiple
+        options={options}
+        value={value1}
+        onChange={o => setValue1(o)}/> */}
+        {/* <Multiselect/> */}
+        <Dropdown/>
+    </>
+    // <Table/>
+    // <List/>
   );
 }
 
